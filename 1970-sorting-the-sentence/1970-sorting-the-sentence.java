1ff57101bd9a59601d22fae7829653d412fa2d93
Sachin Vaishnav <vaishnavsachin286@gmail.com>
2025-06-16 23:47:08 +0530

    Time: 0 ms (100%), Space: 41.8 MB (28.81%) - LeetHub

--- 1970-sorting-the-sentence/1970-sorting-the-sentence.java
+++ 1970-sorting-the-sentence/1970-sorting-the-sentence.java
@@ -7,11 +7,11 @@ class Solution {
             int pos=word.charAt(word.length()-1)-'0';
             res[pos-1]=word.substring(0, word.length()-1);
         }
-        StringBuilder s1=new StringBuilder();
-        for(int i=0;i<res.length;i++){
-            s1.append(res[i]);
-            if(i!=res.length-1) s1.append(" ");
-        }
-        return s1.toString();
+        // StringBuilder s1=new StringBuilder();
+        // for(int i=0;i<res.length;i++){
+        //     s1.append(res[i]);
+        //     if(i!=res.length-1) s1.append(" ");
+        // }
+        return String.join(" ",res);
     }
 }
