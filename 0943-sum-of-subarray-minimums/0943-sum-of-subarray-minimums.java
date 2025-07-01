29c69309fa0b3a161673629c2400aebacfb1191d
Sachin Vaishnav <vaishnavsachin286@gmail.com>
2025-07-02 00:47:19 +0530

    Time: 73 ms (33.35%), Space: 51.1 MB (60.19%) - LeetHub

---
+++ 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.java
@@ -0,0 +1,40 @@
+class Solution {
+   public int sumSubarrayMins(int[] arr) {
+        int mod=(int)(Math.pow(10,9)+7);
+        int []nse=new int[arr.length];
+        nse(arr, nse);
+        int []psee=new int [arr.length];
+        psee(arr, psee);
+        int total=0;
+        for(int i=0;i<arr.length;i++){
+            int left=i-psee[i];
+            int right=nse[i]-i;
+            total=(int)((total+(1l*left*right%mod*arr[i]%mod))%mod);
+        }
+        return total;
+    }
+
+    public int [] nse(int []arr, int []nse){
+        Stack<Integer> stack=new Stack<>();
+        for(int i=arr.length-1;i>=0;i--){
+            while(!stack.isEmpty() && arr[stack.peek()]>=arr[i]){
+                stack.pop();
+            }
+            nse[i]=stack.isEmpty()?arr.length:stack.peek();
+            stack.push(i);
+        }
+        return nse;
+    }
+
+    public int[] psee(int []arr, int []psee){
+        Stack<Integer> stack=new Stack<>();
+        for(int i=0;i<arr.length;i++){
+            while(!stack.isEmpty() && arr[stack.peek()]>arr[i]){
+                stack.pop();
+            }
+            psee[i]=stack.isEmpty()?-1:stack.peek();
+            stack.push(i);
+        }
+        return psee;
+    }
+}
