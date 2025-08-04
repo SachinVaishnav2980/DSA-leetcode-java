9cb864338816a11c5cd7d6686684a525654b3119
Sachin Vaishnav <vaishnavsachin286@gmail.com>
2025-08-05 03:13:02 +0530

    Time: 1 ms (63.69%), Space: 42.5 MB (24.32%) - LeetHub

---
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -0,0 +1,55 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        Deque<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        boolean reverse=false; 
+
+        while (!queue.isEmpty()) {
+            int levelsize = queue.size();
+            List<Integer> currLevel = new ArrayList<>(levelsize);
+            for (int i = 0; i < levelsize; i++) {
+                if(!reverse) {
+                    TreeNode node = queue.pollFirst();
+                    currLevel.add(node.val);
+                    if(node.left != null) {
+                        queue.addLast(node.left);
+                    }
+                    if(node.right != null) {
+                        queue.addLast(node.right);
+                    }
+                }else{
+                    TreeNode node = queue.pollLast();
+                    currLevel.add(node.val);
+                    if(node.right != null) {
+                        queue.addFirst(node.right);
+                    }
+                    if(node.left != null) {
+                        queue.addFirst(node.left);
+                    }
+                }
+            }
+            reverse=!reverse;
+            result.add(currLevel);
+        }
+        return result;
+    }
+}
