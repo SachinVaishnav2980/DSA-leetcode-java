e76b1103d1c24b97066cede32d3fbc92ba327d1d
Sachin Vaishnav <vaishnavsachin286@gmail.com>
2025-08-19 00:16:32 +0530

    Time: 0 ms (100%), Space: 44.9 MB (86.57%) - LeetHub

---
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.java
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int currCount=0;
+    int maxCount=0;
+    TreeNode prev=null;
+    ArrayList<Integer> list=new ArrayList<>();
+    public int[] findMode(TreeNode root) {
+        if(root==null){
+            return new int[]{0};
+        }
+        helper(root, list);
+        int []result=new int[list.size()];
+        for(int i=0;i<list.size();i++){
+            result[i]=list.get(i);
+        }
+        return result;
+    }
+
+    void helper(TreeNode node, ArrayList<Integer> list){
+        if(node==null) return;
+
+        helper(node.left, list);
+
+        if(prev==null || prev.val!=node.val){
+            currCount=1;
+        }else{
+            currCount+=1;
+        }
+
+        if(currCount>maxCount){
+            maxCount=currCount;
+            list.clear();
+            list.add(node.val);
+        }else if(currCount==maxCount){
+            list.add(node.val);
+        }
+
+        prev=node;
+
+        helper(node.right, list);
+    }
+}
