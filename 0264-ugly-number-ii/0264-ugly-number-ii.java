915cb5f4ffd094de9d450adecc2f51827e70fc4a
Sachin Vaishnav <vaishnavsachin286@gmail.com>
2025-09-11 18:08:01 +0530

    Time: 53 ms (15.65%), Space: 44.6 MB (34.18%) - LeetHub

---
+++ 0264-ugly-number-ii/0264-ugly-number-ii.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int nthUglyNumber(int n) {
+        PriorityQueue<Long> pq=new PriorityQueue<>();
+        Set<Long> set=new HashSet<>();
+        pq.add(1L);
+        long curr=1;
+        while (n>0) {
+            curr=pq.poll();
+            n--;
+            //just putting the multiple for future
+            if(!set.contains(curr*2)){
+                set.add(curr*2);
+                pq.add(curr*2);
+            }
+            if(!set.contains(curr*3)){
+                set.add(curr*3);
+                pq.add(curr*3);
+            }
+            if(!set.contains(curr*5)){
+                set.add(curr*5);
+                pq.add(curr*5);
+            }
+        }
+        return (int)curr;
+    }
+}
