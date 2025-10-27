c9ac259249c33f49d9fa9eee980c2429401b6761
Sachin Vaishnav <vaishnavsachin286@gmail.com>
2025-10-28 00:41:58 +0530

    Time: 0 ms (100%), Space: 42.1 MB (78.67%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n=matrix.length;
+        int m=matrix[0].length;
+
+        //Invert the columns 
+        for(int j=0;j<m;j++){
+            for(int i=0;i<n/2;i++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[n-1-i][j];
+                matrix[n-1-i][j]=temp;
+            }
+        }   
+        //swap the elements
+        for(int i=0;i<n;i++){
+            for(int j=i;j<m;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+            }
+        }
+    }
+}
