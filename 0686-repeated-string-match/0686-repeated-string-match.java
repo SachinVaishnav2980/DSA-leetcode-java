0b1e98ad0d75a8464767188eefe65db089a648f9
Sachin Vaishnav <vaishnavsachin286@gmail.com>
2025-10-28 12:16:47 +0530

    Time: 260 ms (66.03%), Space: 42.4 MB (69.63%) - LeetHub

---
+++ 0686-repeated-string-match/0686-repeated-string-match.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int repeatedStringMatch(String a, String b) {
+        int repitition=1;
+        StringBuilder str=new StringBuilder(a);
+        while (str.length()<b.length()) {
+            str.append(a);
+            repitition++;            
+        }
+
+        if(str.indexOf(b)!=-1) return repitition;
+        str.append(a);
+        repitition++;
+        if(str.indexOf(b)!=-1) return repitition;
+
+        return -1;
+    }
+}
