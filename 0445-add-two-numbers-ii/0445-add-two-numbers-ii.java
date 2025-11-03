a9fd3e6331c2cb7281a974480d7b7f51666c2e57
Sachin Vaishnav <vaishnavsachin286@gmail.com>
2025-11-03 19:23:55 +0530

    Time: 4 ms (12.38%), Space: 47.2 MB (6.51%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.java
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.java
@@ -10,16 +10,56 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        // reverse both the Linked lists
-        l1 = reverse(l1);
-        l2 = reverse(l2);
+        Stack<Integer> s1=new Stack<>();
+        Stack<Integer> s2=new Stack<>();    
 
-        // Now add them properly
-        ListNode head2 = sumLL(l1, l2);
+        while (l1!=null) {
+            s1.add(l1.val);
+            l1=l1.next;
+        }
 
-        // again reverse to get final ans
-        head2 = reverse(head2);
-        return head2;
+        while (l2!=null) {
+            s2.add(l2.val);
+            l2=l2.next;
+        }
+
+        ListNode head=null;
+        ListNode temp=null;
+        int carry=0;
+        while (!s1.isEmpty() && !s2.isEmpty()) {
+            int sum=s1.pop()+s2.pop()+carry;
+            if(head==null){
+                head=new ListNode(sum%10);
+                temp=head;
+            }else{
+                ListNode newNode=new ListNode(sum%10);
+                temp.next=newNode;
+                temp=temp.next;
+            }
+            carry=sum/10;
+        }
+        
+        while (!s1.isEmpty()) {
+            int sum=s1.pop()+carry;
+            ListNode newNode=new ListNode(sum%10);
+            temp.next=newNode;
+            temp=temp.next;
+            carry=sum/10;
+        }
+
+        while (!s2.isEmpty()) {
+            int sum=s2.pop()+carry;
+            ListNode newNode=new ListNode(sum%10);
+            temp.next=newNode;
+            temp=temp.next;
+            carry=sum/10;
+        }
+
+        if(carry!=0){
+            ListNode newNode=new ListNode(carry);
+            temp.next=newNode;
+        }
+        return reverse(head);
     }
 
     ListNode reverse(ListNode head) {
@@ -32,51 +72,4 @@ class Solution {
         }
         return prev;
     }
-
-    ListNode sumLL(ListNode l1, ListNode l2){
-            ListNode head2=null;
-            ListNode temp3=null;
-
-            int carry=0;
-
-            while (l1!=null && l2!=null) {
-                int sum=(l1.val+l2.val)+carry;
-
-                if(head2==null){
-                    head2=new ListNode(sum%10);
-                    temp3=head2;
-                }else{
-                    ListNode node=new ListNode(sum%10);
-                    temp3.next=node;
-                    temp3=temp3.next;
-                }
-                carry=sum/10;
-                l1=l1.next;
-                l2=l2.next;
-            }
-
-            while (l1!=null) {
-                int sum=(l1.val)+carry;
-                ListNode node=new ListNode(sum%10);
-                temp3.next=node;
-                temp3=temp3.next;
-                carry=sum/10;
-                l1=l1.next;
-            }
-
-            while (l2!=null) {
-                int sum=(l2.val)+carry;
-                ListNode node=new ListNode(sum%10);
-                temp3.next=node;
-                temp3=temp3.next;
-                carry=sum/10;
-                l2=l2.next;
-            }
-
-            if(carry!=0){
-                ListNode newNode=new ListNode(carry);
-                temp3.next=newNode;
-            }
-            return head2;
-        }
 }
