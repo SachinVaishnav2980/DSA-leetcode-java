bdd38e4f915a8acf6d50f785c17dc8faeb240865
Sachin Vaishnav <vaishnavsachin286@gmail.com>
2025-11-03 15:23:59 +0530

    Time: 1 ms (100%), Space: 46.3 MB (5.52%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -10,41 +10,48 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode dummyNode=new ListNode(0);
-        ListNode current=dummyNode;
+        ListNode head2=null;
+        ListNode temp;
+        temp=head2;
+
         int carry=0;
-        int sum;
-        while(l1!=null && l2!=null){
-            sum=0;
-            sum=l1.val+l2.val+carry;
-            int digit=sum%10;
-            current.next=new ListNode(digit);
-            current=current.next;
-            carry= Math.floorDiv(sum,10);
-            l1=l1.next;
-            l2=l2.next;
-        }
-        while(l1!=null){
-            sum=0;
-            sum=l1.val+carry;
-            int digit=sum%10;
-            current.next=new ListNode(digit);
-            current=current.next;
-            carry= Math.floorDiv(sum,10);
-            l1=l1.next;
-        }
-        while(l2!=null){
-            sum=0;
-            sum=l2.val+carry;
-            int digit=sum%10;
-            current.next=new ListNode(digit);
-            current=current.next;
-            carry= Math.floorDiv(sum,10);
-            l2=l2.next;
-        }
-        if(carry==1){
-            current.next=new ListNode(carry);
-        }
-        return dummyNode.next;
+        while (l1!=null && l2!=null) {
+                int sum=(l1.val+l2.val)+carry;
+
+                if(head2==null){
+                    head2=new ListNode(sum%10);
+                    temp=head2;
+                }else{
+                    ListNode node=new ListNode(sum%10);
+                    temp.next=node;
+                    temp=temp.next;
+                }
+                carry=sum/10;
+                l1=l1.next;
+                l2=l2.next;
+            }
+
+            while (l1!=null) {
+                int sum=(l1.val)+carry;
+                ListNode node=new ListNode(sum%10);
+                temp.next=node;
+                temp=temp.next;
+                carry=sum/10;
+                l1=l1.next;
+            }
+
+            while (l2!=null) {
+                int sum=(l2.val)+carry;
+                ListNode node=new ListNode(sum%10);
+                temp.next=node;
+                temp=temp.next;
+                carry=sum/10;
+                l2=l2.next;
+            }
+            if(carry!=0){
+                ListNode newNode =new ListNode(carry);
+                temp.next=newNode;
+            }
+        return head2;
     }
 }
