307eaab19a461cce1ff847f688c39d52162528ab
Sachin Vaishnav <vaishnavsachin286@gmail.com>
2025-11-10 14:26:44 +0530

    Time: 7 ms (67.69%), Space: 44.9 MB (28.58%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,12 +1,42 @@
 class Solution {
+    static int MOD = 1000000007;
     public int findTargetSumWays(int[] nums, int target) {
-        return helper(nums, target, 0, 0);
+        int n=nums.length;
+        int totalSum=0;
+        for(int element:nums) totalSum+=element;
+        //Edge cases
+        
+        if (Math.abs(target) > totalSum) return 0;
+        if ((totalSum + target) % 2 != 0) return 0;
+        int sum = (totalSum + target) / 2;
+
+
+        int [][]t=new int [n+1][sum+1];
+
+        return subsetSum(t, nums, sum);
     }
-    private int helper(int[] arr, int target, int idx, int sum) {
-        if (idx >= arr.length)
-            return sum == target ? 1 : 0;
-        int subtract = helper(arr, target, idx + 1, sum - arr[idx]);
-        int add = helper(arr, target, idx + 1, sum + arr[idx]);
-        return subtract + add;
-    }
+
+        int subsetSum(int [][]t, int []nums, int sum){
+            //DP initialization
+            int n=nums.length;
+            for(int i=0;i<=n;i++){
+                t[i][0]=1;
+            }
+
+            for(int j=1;j<=sum;j++){
+                t[0][j]=0;
+            }
+
+            //Solving method
+            for(int i=1;i<=n;i++){
+                for(int j=0;j<=sum;j++){
+                    if(nums[i-1]<=j){
+                        t[i][j]=(int) ((t[i - 1][j - nums[i - 1]] + (long)t[i - 1][j]) % MOD);
+                    }else{
+                        t[i][j]=t[i-1][j];
+                    }
+                }
+            }
+            return t[n][sum];
+        }
 }
