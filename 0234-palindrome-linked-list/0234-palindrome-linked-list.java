0c2774894309197975d004b2916c8e6aa9d890e3
Sachin Vaishnav <vaishnavsachin286@gmail.com>
2025-11-11 12:26:05 +0530

    Time: 11 ms (17.46%), Space: 99.6 MB (5.16%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -9,48 +9,28 @@
  * }
  */
 class Solution {
-    public Boolean isPalindrome(ListNode head){
-        ListNode mid=middleNode(head);
-        ListNode head2=reverseIter(mid);
-        ListNode head3=head2;
-        while(head!=null && head2!=null){
-            if(head.val!=head2.val){
-                break;
-            }
-            head=head.next;
-            head2=head2.next;
+    public boolean isPalindrome(ListNode head) {
+        ListNode temp=head;
+        List<Integer> list =new ArrayList<>();
+        while (temp!=null) {
+            list.add(temp.val);
+            temp=temp.next;
         }
-        reverseIter(head3);
-        if(head==null || head2==null){
-            return true;
-        }
-        return false;
-    }
-    public ListNode middleNode(ListNode head) {
-        ListNode fast=head;
-        ListNode slow= head;
-        while(fast!=null && fast.next!=null){
-            slow=slow.next;
-            fast=fast.next.next;
-        } 
-        return slow;
-    }
-    public ListNode reverseIter(ListNode head){
-            if (head == null) {
-            return head;
-        }
-        ListNode prev = null;
-        ListNode present = head;
-        ListNode next = present.next;
 
-        while (present != null) {
-            present.next = prev;
-            prev = present;
-            present = next;
-            if (next != null) {
-                next = next.next;
+        //compare both arrays
+        return compare(list);
+    }
+
+    boolean compare(List<Integer> list){
+        int i=0;
+        int j=list.size()-1;
+        while (i<j) {
+            if(!list.get(i).equals(list.get(j))){
+                return false;
             }
+            i++;
+            j--;
         }
-        return prev;
-        }
+        return true;
+    }
 }
