900c6fe324998c39635647c5716216ff4e1b32ee
Sachin Vaishnav <vaishnavsachin286@gmail.com>
2025-12-04 12:35:09 +0530

    Time: 10 ms (93.15%), Space: 47.6 MB (26.61%) - LeetHub

--- 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.java
+++ 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.java
@@ -1,47 +1,21 @@
 class Solution {
+    //Two pointer approach
     public int countCollisions(String directions) {
-        Stack<Character> st=new Stack<>();
-        int coll=0;
-        //iterate and find curr dir for comparing with prev
-        for(char ch:directions.toCharArray()){
-            if(st.isEmpty()){st.push(ch); continue;}
-            //if current dir is left
-            if(ch=='L'){
-                if(st.peek()=='R'){
-                    //pop all left R
-                    int cntr=0;
-                    while(!st.isEmpty() && st.peek()=='R'){
-                        cntr++;
-                        st.pop();
-                    }
-                    coll+=cntr+1;
-                    if(st.isEmpty() || st.peek()!='S') st.push('S');
-                }else if(st.peek()=='S'){
-                    coll+=1;
-                    st.push('S');
-                }else{
-                    st.push('L');
-                }
-            }
-            //if current dir is stop
-            else if(ch=='S'){
-                if(st.peek()=='R'){
-                    int cntr = 0;
-                    while (!st.isEmpty() && st.peek() == 'R') {
-                        st.pop();
-                        cntr++;
-                    }
-                    coll+=cntr;
-                    if(st.isEmpty() || st.peek()!='S') st.push('S');
-                }
-                else{
-                    st.push('S');
-                }
-            }
-            else{
-                st.push(ch);
-            }
+        int n = directions.length();
+        int i = 0;
+        // skip leading 'L' cars (they never collide)
+        while (i < n && directions.charAt(i) == 'L') i++;
+
+        int j = n - 1;
+        
+        // skip trailing 'R' cars (they never collide)
+        while (j >= 0 && directions.charAt(j) == 'R') j--;
+
+        if (i > j) return 0; // crossed
+        int collisions = 0;
+        for (int k = i; k <= j; k++) {
+            if (directions.charAt(k) != 'S') collisions++;
         }
-        return coll;
+        return collisions;
     }
 }
