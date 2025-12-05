b4d02eeaa9894ef44817dbe489dd76480c396e9e
Sachin Vaishnav <vaishnavsachin286@gmail.com>
2025-12-06 01:03:52 +0530

    Time: 0 ms (100%), Space: 41.9 MB (40%) - LeetHub

---
+++ 0991-broken-calculator/0991-broken-calculator.java
@@ -0,0 +1,11 @@
+class Solution {
+    //Greedy approach
+    public int brokenCalc(int startValue, int target) {
+        if(startValue>=target) return startValue-target; //greed
+
+        if(target%2==0){
+            return 1+brokenCalc(startValue, target/2);
+        }
+        return 1+brokenCalc(startValue, target+1);
+    }
+}
