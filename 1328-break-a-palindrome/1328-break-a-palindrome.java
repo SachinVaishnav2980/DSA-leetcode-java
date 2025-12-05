eb618cc1c1981aeb57411060e02a73feb025eb9b
Sachin Vaishnav <vaishnavsachin286@gmail.com>
2025-12-05 23:27:24 +0530

    Time: 0 ms (100%), Space: 42.5 MB (21.65%) - LeetHub

---
+++ 1328-break-a-palindrome/1328-break-a-palindrome.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String breakPalindrome(String palindrome) {
+        int n=palindrome.length();
+        if(n==1) return "";
+        StringBuilder str=new StringBuilder(palindrome);
+
+        for(int i=0;i<n/2;i++){
+            char ch=str.charAt(i);
+
+            if(ch!='a'){
+                str.setCharAt(i, 'a');
+                return str.toString();
+            }
+        }
+
+        str.setCharAt(n-1, 'b');
+        return str.toString();
+    }
+}
