7aa204115552b4fb1e58ee984770f00cd01a19c6
Sachin Vaishnav <vaishnavsachin286@gmail.com>
2025-12-18 20:31:59 +0530

    Time: 2 ms (40.39%), Space: 43.3 MB (45.55%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
@@ -1,32 +1,31 @@
 class Solution {
-    int []dp=new int[366];
     public int mincostTickets(int[] days, int[] costs) {
-        Arrays.fill(dp, -1);
-        return solve(days, costs, 0);
-    }
+        int n=days.length;
+        int []dp=new int[n+1];
+        dp[n]=0;
+        for(int i=n-1;i>=0;i--){
 
-    int solve(int []days, int []costs, int index){
-        if(index>=days.length) return 0;
-        if(dp[index]!=-1) return dp[index];
-        int j=index;
-        //1-Day pass
-        while(j<days.length && days[j]<days[index]+1){
-            j++;
+            int j=i;
+            //1-Day pass
+            while(j<days.length && days[j]<days[i]+1){
+                j++;
+            }
+            int oneday=costs[0]+dp[j];
+
+            //7-Day pass
+            while(j<days.length && days[j]<days[i]+7){
+                j++;
+            }
+            int sevenday=costs[1]+dp[j];
+
+            //30-Day pass
+            while(j<days.length && days[j]<days[i]+30){
+                j++;
+            }
+            int thirtyday=costs[2]+dp[j];
+
+            dp[i]=Math.min(oneday, Math.min(sevenday, thirtyday));
         }
-        int oneday=costs[0]+solve(days, costs, j);
-
-        //7-Day pass
-        while(j<days.length && days[j]<days[index]+7){
-            j++;
-        }
-        int sevenday=costs[1]+solve(days, costs, j);
-
-        //30-Day pass
-        while(j<days.length && days[j]<days[index]+30){
-            j++;
-        }
-        int thirtyday=costs[2]+solve(days, costs, j);
-
-        return dp[index]=Math.min(oneday, Math.min(sevenday, thirtyday));
+        return dp[0];
     }
 }
