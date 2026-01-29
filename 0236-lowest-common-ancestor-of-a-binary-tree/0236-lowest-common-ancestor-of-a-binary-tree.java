04a3244a7119f98ebac6b289b709278c66942390
Sachin Vaishnav <vaishnavsachin286@gmail.com>
2026-01-29 14:49:49 +0530

    Time: 7 ms (98.81%), Space: 46.6 MB (35.92%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -12,17 +12,14 @@ class Solution {
         if(root==null){
             return null;
         }
-
         if(root==p || root==q){
             return root;
-        } 
+        }
         TreeNode left=lowestCommonAncestor(root.left, p, q);
         TreeNode right=lowestCommonAncestor(root.right, p, q);
-
         if(left!=null && right!=null){
             return root;
         }
-
-        return left==null?right:left;
+        return (left!=null)?left:right;      
     }
 }
