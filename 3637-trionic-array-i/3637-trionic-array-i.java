8dfeb7a49fd0c4a92d9fe75c8ea4b54f72a7b12a
Sachin Vaishnav <vaishnavsachin286@gmail.com>
2026-02-03 13:54:52 +0530

    Time: 0 ms (100%), Space: 44.9 MB (10.08%) - LeetHub

--- 3637-trionic-array-i/3637-trionic-array-i.java
+++ 3637-trionic-array-i/3637-trionic-array-i.java
@@ -2,35 +2,28 @@ class Solution {
     public boolean isTrionic(int[] nums) {
         int n=nums.length;
 
-        for(int p=1;p<n-1;p++){
-            for(int q=p+1;q<n-1;q++){
-                boolean first=strictlyIncreasing(nums, 0, p);
-                boolean second=strictlyDecreasing(nums, p, q);
-                boolean third=strictlyIncreasing(nums, q, n-1);
+        if(n<4) return false;
 
-                if(first && second && third){
-                    return true;
-                }
-            }
+        int i=1;
+
+        //strictly increasing
+        while(i<n && nums[i]>nums[i-1]){
+            i++;
         }
-        return false;
+
+        if(i==1 || i==n) return false;
+
+        //strictly decreasing
+        while(i<n && nums[i]<nums[i-1]){
+            i++;
+        }
+        if(i==n) return false;
+
+        //strictly increasing
+        while(i<n && nums[i]>nums[i-1]){
+            i++;
+        }
+        return i==n;
     }
 
-    boolean strictlyIncreasing(int []nums, int p, int q){
-        for(int i=p;i<q;i++){
-            if(nums[i]>=nums[i+1]){
-                return false;
-            }
-        }
-        return true;
-    }
-
-    boolean strictlyDecreasing(int []nums, int p, int q){
-        for(int i=p;i<q;i++){
-            if(nums[i]<=nums[i+1]){
-                return false;
-            }
-        }
-        return true;
-    }
 }
