0deb534c6521aab3a4cf8ca7504508305927b3ed
Sachin Vaishnav <vaishnavsachin286@gmail.com>
2026-02-15 16:57:39 +0530

    Time: 8 ms (20.94%), Space: 47.8 MB (27.58%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -0,0 +1,49 @@
+class Solution {
+    public int[] findOrder(int N, int[][] edges) {
+        // Your Code goes here
+        List<List<Integer>> adj=new ArrayList<>();
+        for(int i=0;i<N;i++){
+            adj.add(new ArrayList<>());
+        }
+        //prepare list
+        for(int []e:edges){
+            int u=e[0];
+            int v=e[1];
+            adj.get(u).add(v);
+        }
+        int []indegree=new int[N];
+        
+        //calculate indegree
+        for(int i = 0; i < N; i++){
+            for(int neighbor : adj.get(i)){
+                indegree[neighbor]++;
+            }
+        }
+        
+        Queue<Integer> queue=new LinkedList<>();
+        for(int i=0;i<N;i++){
+            if(indegree[i]==0){
+                queue.add(i);
+            }
+        }
+        
+        List<Integer> topo=new ArrayList<>();
+        while(!queue.isEmpty()){
+            int node=queue.poll();
+            topo.add(node);
+            
+            for(int el:adj.get(node)){
+                indegree[el]--;
+                if(indegree[el]==0) queue.add(el);
+            }
+        }
+        if(topo.size()==N){
+            int []ans=new int[N];
+            for(int i=0;i<N;i++){
+                ans[i]=topo.get(N-1-i);
+            }
+            return ans;
+        }
+        return new int[]{};
+    }
+}
