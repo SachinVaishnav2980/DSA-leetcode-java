e939e034f2ebc24ff2353999609764e950a32c42
Sachin Vaishnav <vaishnavsachin286@gmail.com>
2026-04-01 19:59:12 +0530

    Time: 40 ms (94.85%), Space: 44.3 MB (73.82%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.java
+++ 0312-burst-balloons/0312-burst-balloons.java
@@ -1,29 +1,34 @@
 class Solution {
     public int maxCoins(int[] nums) {
-        int n=nums.length;
+        int n = nums.length;
 
-        int []newNums=new int[n+2];
-        newNums[0]=1;
-        newNums[n+1]=1;
-        for(int i=0;i<n;i++){
-            newNums[i+1]=nums[i];
+        int[] newNums = new int[n + 2];
+        newNums[0] = 1;
+        newNums[n + 1] = 1;
+
+        for (int i = 0; i < n; i++) {
+            newNums[i + 1] = nums[i];
         }
-        int [][]dp=new int [301][301];
-        int max=helper(1, nums.length, newNums, dp);
-        return max;
-    }
 
-    private int helper(int left, int right, int []nums, int [][]dp){
-        //Base case
-        if(left>right) return 0;
+        int[][] dp = new int[n + 2][n + 2];
 
-        //Hypothesis
-        if(dp[left][right]!=0) return dp[left][right];
-        int ans = Integer.MIN_VALUE;
-        for(int i=left; i<=right; i++){
-           int coins= nums[left-1] * nums[i] * nums[right+1] + helper(left,i-1,nums, dp) + helper(i+1,right,nums, dp);
-           ans=Math.max(ans, coins);
+        for (int left = n; left >= 1; left--) {
+            for (int right = left; right <= n; right++) {
+
+                int ans = Integer.MIN_VALUE;
+
+                for (int i = left; i <= right; i++) {
+                    int coins = newNums[left - 1] * newNums[i] * newNums[right + 1]
+                              + dp[left][i - 1]
+                              + dp[i + 1][right];
+
+                    ans = Math.max(ans, coins);
+                }
+
+                dp[left][right] = ans;  
+            }
         }
-        return dp[left][right]=ans;        
+
+        return dp[1][n];
     }
 }
